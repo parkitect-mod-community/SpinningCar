75d1b6b53a52d3dcde6cdef0a5167878a3d76439
Michael Pollind <mpollind@gmail.com>
2016-07-10 14:23:49 -0700

    start

---
+++ AssetBundleManager.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class AssetBundleManager
+{
+	private Main Main {get;set;}
+    public GameObject Car;
+
+	public AssetBundleManager (Main main)
+	{
+		this.Main = main;
+        Car = LoadAsset<GameObject> ("spinningCar");
+    }
+
+
+
+	private T  LoadAsset<T>(string prefabName) where T : UnityEngine.Object
+	{
+		try
+		{
+			T asset;
+
+			char dsc = System.IO.Path.DirectorySeparatorChar;
+            using (WWW www = new WWW("file://" + Main.Path + dsc + "assetbundle" + dsc + "spinningCar"))
+			{
+
+				if (www.error != null)
+				{
+					Debug.Log("Loading had an error:" + www.error);
+					throw new Exception("Loading had an error:" + www.error);
+				}
+				if(www.assetBundle == null)
+				{
+					Debug.Log("Loading had an error:" + www.error);
+					throw new Exception("assetBundle is null");
+
+				}
+				AssetBundle bundle = www.assetBundle;
+
+
+				try
+				{
+					asset = bundle.LoadAsset<T>(prefabName);
+					bundle.Unload(false);
+
+					return asset;
+				}
+				catch (Exception e)
+				{
+					UnityEngine.Debug.LogException(e);
+					bundle.Unload(false);
+					return null;
+				}
+			}
+		}
+		catch (Exception e)
+		{
+			UnityEngine.Debug.LogException(e);
+			return null;
+		}
+	}
+}
+
+
---
+++ Main.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using TrackedRiderUtility;
+
+public class Main : IMod
+{
+    public string Identifier { get; set; }
+	public static AssetBundleManager AssetBundleManager = null;
+  
+    private TrackRiderBinder binder;
+
+    public void onEnabled()
+    {
+        
+		if (Main.AssetBundleManager == null) {
+
+			AssetBundleManager = new AssetBundleManager (this);
+		}
+
+        binder = new TrackRiderBinder ("248fd3fdc996afcc56102bf4a8d456d7");
+
+        CoasterCarInstantiator coasterCarInstantiator = binder.RegisterCoasterCarInstaniator<CoasterCarInstantiator> (TrackRideHelper.GetTrackedRide("Mini Coaster"), "SpinningCarInstantiator", "Spinning Car", 1, 1, 1);
+        SpinningCar spinningCar =  binder.RegisterCar<SpinningCar> ( Main.AssetBundleManager.Car,"SpinningCar", .3f,.1f,true, new Color[] { 
+            new Color(71f / 255, 71f / 255, 71f / 255), 
+            new Color(176f / 255, 7f / 255, 7f / 255), 
+            new Color(26f / 255, 26f / 255, 26f / 255)}
+        );
+        coasterCarInstantiator.carGO = spinningCar.gameObject;
+        coasterCarInstantiator.carGO.AddComponent<RestraintRotationController>().closedAngles = new Vector3(0,0,120);
+
+        binder.Apply ();
+
+
+	}
+
+   
+
+
+
+    public void onDisabled()
+    {
+        binder.Unload ();
+	}
+
+    public string Name
+    {
+        get { return "Spinning Car"; }
+    }
+
+    public string Description
+    {
+        get { return "The Virginia reel  uses side friction like tracks. The tubs, have inward facing seating which spin freely on a chassis. The tubs are spun as they contacted the edges of the trough."; }
+    }
+
+
+	public string Path { get; set; }
+
+}
+
